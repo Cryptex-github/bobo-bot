6cb6175abd83422b98ab0b9b7045601bdd6f36f3
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-08 19:08:11 -0800

    await get_messages

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -6,7 +6,7 @@ from core import Cog
 class Listeners(Cog):
     @Cog.listener()
     async def on_raw_message_delete(self, payload: discord.RawMessageDeleteEvent) -> None:
-        if messages := self.bot.delete_message_manager.get_messages(payload.message_id):
+        if messages := await self.bot.delete_message_manager.get_messages(payload.message_id):
             try:
                 await self.bot.http.delete_messages(payload.channel_id, messages) # Well if someone were to edit their message 100 times then uhh idk.
             except (discord.Forbidden, discord.NotFound):
