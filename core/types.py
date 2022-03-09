6c96bb57ffcf1dff2a5024c93fe9399327407210
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-08 18:31:19 -0800

    work

--- core/types.py
+++ core/types.py
@@ -1,10 +1,6 @@
-from __future__ import annotations
+from typing import Union, Tuple, Dict, Any
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from typing import Union, Tuple, Dict, Any
-    from discord import Embed, File
+from discord import Embed, File
 
 __all__ = ('OUTPUT_TYPE',)
 
