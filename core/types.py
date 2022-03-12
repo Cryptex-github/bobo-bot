c3dc550cdb836ba01f0efc8179efd441ec2147f1
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-11 19:20:10 -0800

    RTFM commands

--- core/types.py
+++ core/types.py
@@ -1,8 +1,10 @@
-from typing import Union, Tuple, Dict, Any
+from typing import TypeAlias, Union, Tuple, Dict, Any, Literal
 
 from discord import Embed, File
 
-__all__ = ('OUTPUT_TYPE',)
+__all__ = ('OUTPUT_TYPE', 'POSSIBLE_RTFM_SOURCES')
 
 
-OUTPUT_TYPE = Union[Tuple[Union[Embed, str, File, Dict[str, Any], bool]], Union[Embed, str, File, Dict[str, Any], bool]]
+OUTPUT_TYPE: TypeAlias = Union[Tuple[Union[Embed, str, File, Dict[str, Any], bool], ...], Union[Embed, str, File, Dict[str, Any], bool]]
+
+POSSIBLE_RTFM_SOURCES: TypeAlias = Literal['python', 'asyncpg', 'rust', 'discord', 'discord_master', 'crates']
