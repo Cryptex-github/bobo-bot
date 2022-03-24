9c22235a817bf08220a11fc98e387aaa6097d31c
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-24 12:58:09 -0700

    also match files with _

--- core/constants.py
+++ core/constants.py
@@ -6,7 +6,7 @@ __all__ = ('Regexs',)
 
 
 class Regexs:
-    FILES_TO_RELOAD_REGEX = re.compile(r'\w+/[a-z]+\.py')
+    FILES_TO_RELOAD_REGEX = re.compile(r'\w+/[a-z_]+\.py')
     SPHINX_ENTRY_REGEX = re.compile(r"(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)")
     URL_REGEX = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
