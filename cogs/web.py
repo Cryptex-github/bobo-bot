232728e439fe57501f085ffd383fbac4ab195dd0
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-30 14:36:51 -0700

    don't sort keys

--- cogs/web.py
+++ cogs/web.py
@@ -1,4 +1,4 @@
-from quart import Quart
+from quart import Quart, jsonify
 from quart_cors import cors
 
 app = Quart(__name__)
@@ -16,7 +16,7 @@ async def stats():
     most_used_command = await app.bot.db.fetchval('SELECT command FROM commands_usage ORDER BY uses DESC LIMIT 1')
     latency = await app.bot.self_test()
 
-    return {
+    return jsonify({
         'Servers': len(app.bot.guilds),
         'Users': len(app.bot.users),
         'Channels': len(list(app.bot.get_all_channels())),
@@ -27,7 +27,7 @@ async def stats():
         'Redis Latency': f'{latency.redis} ms',
         'Discord REST Latency': f'{latency.discord_rest} ms',
         'Discord WebSocket Latency': f'{latency.discord_ws} ms',
-    }
+    }, sort_keys=False)
 
 
 async def setup(bot):
